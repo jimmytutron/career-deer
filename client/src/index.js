afb56857249541cfcf03d867f6aafca59f416e35
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-06-30 00:45:33 -0700

    Add immutable js and redux-immutable

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,26 @@
+// Vendor
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
+// This gives our app access to the store
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Core app
+import App from './containers/App/App';
+
+// Import our store configuration.
+// We've extracted the logic into a configureStore.js
+// file to keep this index.js cleaner and more focused on
+// what it needs to be concerned with.
+import configureStore from './configureStore';
+
+const initialState = {};
+const store = 
+
+ReactDOM.render(
+
+<App />,
+ document.getElementById('root')
+);
 registerServiceWorker();
