75b785753b4637f70197ad710e66902c61fac6b3
jimmytu <jimmytutron@gmail.com>
2018-07-01 16:37:48 -0700

    Full screen landing jumbotron

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,46 +1,34 @@
 import React from "react";
-
-// const Nav = ({ children, className, ...props }) => (
-//   <nav className={`navbar ${className}`} {...props}>
-//     {children}
-//   </nav>
-// );
+import "./nav.css";
 
 const Nav = ({className="", children, ...props}) => (
 
-<nav className={`navbar navbar-expand-lg navbar-light bg-light ${className}`} {...props}>
-  <a className="navbar-brand" href="#">Career Deer</a>
+<nav className={`navbar sticky-top navbar-expand-lg navbar-light bg-light ${className}`} {...props}>
+  <a className="navbar-brand" href="#"><img className="logo" src="imgs/logo-horizontal.svg"/></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+    <ul className="navbar-nav ml-auto w-100 justify-content-end">
+      <li className="nav-item">
+        <a className="nav-link" href="/">Home</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
+        <a className="nav-link" href="">Another Link</a>
       </li>
       <li className="nav-item dropdown">
         <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Dropdown
         </a>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item" href="#">Action</a>
-          <a className="dropdown-item" href="#">Another action</a>
+        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item" href="/login">Sign Up</a>
+          <a className="dropdown-item" href="/login">Another action</a>
           <div className="dropdown-divider"></div>
-          <a className="dropdown-item" href="#">Something else here</a>
+          <a className="dropdown-item" href="login">Something else here</a>
         </div>
       </li>
-      <li className="nav-item">
-        <a className="nav-link disabled" href="#">Disabled</a>
-      </li>
     </ul>
-    <form className="form-inline my-2 my-lg-0">
-      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
 
