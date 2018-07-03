7dcddadd5df1ecbe10a2f72d941412d1b3ab4f70
jimmytu <jimmytutron@gmail.com>
2018-07-02 22:58:59 -0700

    Add about blurb

--- client/src/components/HomePageJumbo/HomePageJumbo.js
+++ client/src/components/HomePageJumbo/HomePageJumbo.js
@@ -4,15 +4,18 @@ import {Col, Row} from "../Grid";
 
 const HomePageJumbo = () => (
   <Row className="justify-content-center home-jumbotron p-0">
-  <Col size="12 md-7" className="pl-5 pt-5">
-    <h1 className="display-3 font-weight-bold">Don't be a deer in headlights...</h1>
-    <p className="lead">Track down your next job with CareerDeer</p>
-    <p>pew pew pew. lorem ipsum deer bucks tracks antlers rudolph the red nose reindeer had a very shiny nose.</p>
+  <Col size="12 md-6" className="pl-5">
+    <h1 className="display-4 font-weight-bold">Don't be a deer in headlights...</h1>
+    <h4 className="font-italic">Track down your next job with CareerDeer</h4>
+    
+    <p>We’ll turn you from the hunted into the hunter and help you track down the job of your dreams. The job hunt is a challenging and scary process. There’s a lot of names to keep track of and even more dates. We provide you with a single location to organize and schedule your progress. There's even job search functionality so you can look at job listings without leaving our site and get reminders when your interviews are coming up.
+    </p>
+
     <p className="lead">
-      <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      <a className="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
     </p>
   </Col>
-  <Col size="12 md-5" className="m-0">
+  <Col size="12 md-4" className="m-0">
   </Col>
   </Row>
 );
