543d5a292ceca47d74e771852ef35c5040ba35d9
jimmytu <jimmytutron@gmail.com>
2018-07-02 18:06:44 -0700

    Modify images, convert divs to components

--- client/src/components/HomePageJumbo/HomePageJumbo.js
+++ client/src/components/HomePageJumbo/HomePageJumbo.js
@@ -1,19 +1,20 @@
 import React from "react";
 import './HomePageJumbo.css';
+import {Col, Row} from "../Grid";
 
 const HomePageJumbo = () => (
-  <div className="row justify-content-center home-jumbotron p-0">
-  <div className="col-12 col-md-7 pl-5 pt-5">
+  <Row className="justify-content-center home-jumbotron p-0">
+  <Col size="12 md-7" className="pl-5 pt-5">
     <h1 className="display-3 font-weight-bold">Don't be a deer in headlights...</h1>
     <p className="lead">Track down your next job with CareerDeer</p>
     <p>pew pew pew. lorem ipsum deer bucks tracks antlers rudolph the red nose reindeer had a very shiny nose.</p>
     <p className="lead">
       <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
     </p>
-  </div>
-  <div className="col-12 col-md-5 m-0">
-  </div>
-  </div>
+  </Col>
+  <Col size="12 md-5" className="m-0">
+  </Col>
+  </Row>
 );
 
 export {HomePageJumbo};
