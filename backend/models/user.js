85f757542066f1c8930397fea4a6696353c3ee4d
Robert Shaw <robertshaw87@gmail.com>
2018-07-05 11:37:36 -0700

    fiddle more with authenticate method

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,15 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
+//require bcrypt for password
+var bcrypt = require("bcrypt-nodejs");
 
 const userSchema = new Schema({
-  name: {
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
     type: String,
     required: true
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   password: {
     type: String,
@@ -18,6 +25,10 @@ const userSchema = new Schema({
 })
 
 userSchema.plugin(passportLocalMongoose);
+
+userSchema.methods.authenticate = function(password) {
+  return bcrypt.compareSync(password, this.password)
+}
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
