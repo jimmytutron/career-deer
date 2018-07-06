db0b7f59d5050b253b68452f8ce86ab66b61b811
jimmytu <jimmytutron@gmail.com>
2018-07-05 19:04:44 -0700

    Add more info to home page with icons and animations

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -8,6 +8,12 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { updateTest } from '../../actions/test-actions';
 import { updateApp } from '../../actions/app-action';
+import './Home.css';
+
+import Rotate from 'react-reveal/Rotate';
+import Zoom from 'react-reveal/Zoom';
+import LightSpeed from 'react-reveal/LightSpeed';
+import Slide from 'react-reveal/Slide';
 
 class Home extends Component {
 
@@ -20,15 +26,48 @@ class Home extends Component {
     return (
       <React.Fragment>
         <HomePageJumbo />
-        <Container className="mb-3">
-          <Row>
-            <Col size="2 lg-4" className="bg-primary py-5">Banana</Col>
-            <Col size="8 lg-4" className="bg-light py-5">Chicken</Col>
-            <Col size="2 lg-4" className="bg-danger py-5" id="test">Bacon</Col>
+        <div className="homepage">
+        <Slide bottom>
+        <Row className="justify-content-center my-3">
+        <Col size="12">
+        <h1 className="display-3 text-center font-weight-bold">Makin' it Rein...</h1>
+        <p>
+        </p>
+        </Col>
+        </Row>
+          <Row className="justify-content-center mb-3">
+            <Col size="12 md-4 lg-3">
+            <Rotate duration={1500}>
+            <img className="target mx-auto my-3" src="/imgs/icons/circular-target.svg"/>
+            </Rotate>
+            <h1 className="display-6 text-center">Hunt down the job</h1>
+            <p className="px-5">
+            YOLO vice plaid messenger bag, af palo santo poutine offal fixie you probably haven't heard of them. Etsy scenester kinfolk forage asymmetrical meh paleo. Cred scenester next level neutra raclette. Scenester succulents leggings portland, air plant raw denim swag four dollar toast health goth.
+            </p>
+            </Col>
+            <Col size="12 md-4 lg-3">
+            <Zoom duration={1500}>
+            <img className="target mx-auto my-3" src="/imgs/icons/check-box.svg"/>
+            </Zoom>
+            <h1 className="display-6 text-center">Track your progress</h1>
+            <p className="px-5">
+            Lorem ipsum dolor amet keytar wolf post-ironic swag franzen lyft. Vinyl tattooed tumeric shabby chic, adaptogen cliche meh disrupt snackwave. Cred scenester next level neutra raclette. Scenester succulents leggings portland, air plant raw denim swag four dollar toast health goth.
+            </p>
+            </Col>
+            <Col size="12 md-4 lg-3">
+            <LightSpeed right duration={1500}>
+            <img className="target mx-auto my-3" src="/imgs/icons/dart-board.svg"/>
+            </LightSpeed>
+            <h1 className="display-6 text-center">Hit your target</h1>
+            <p className="px-5">
+            You probably haven't heard of them scenester cloud bread, vape disrupt trust fund food truck pok pok. Cred scenester next level neutra raclette. Scenester succulents leggings portland, air plant raw denim swag four dollar toast health goth.
+            </p>
+            </Col>
           </Row>
-        </Container>
-          <button className="btn btn-primary" onClick={() => this.onUpdateTest()}>Update Test</button>
-          {this.props.test}
+          </Slide>
+          </div>
+          {/*<button className="btn btn-primary" onClick={() => this.onUpdateTest()}>Update Test</button>
+          {this.props.test}*/}
       </React.Fragment>
     );
   }
