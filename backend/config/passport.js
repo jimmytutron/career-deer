7e5710ba42211f774d6e7daa63219786cc0c7c27
Robert Shaw <robertshaw87@gmail.com>
2018-07-06 00:11:20 -0700

    revert changes

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,51 +1,40 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+// console.log(LocalStrategy)
 
-const User = require('../models/user');
-
-passport.use(new LocalStrategy(User.authenticate()))
-//   {
-//     //Updating username field to email rather than default "username" from LocalStrategy
-//     usernameField: 'email'
-//   },
-//   async function (email, password, done) {
-//     console.log("bababababababbababa")
-//     console.log(await db.User.authenticate()('robertshaw87@gmil.com', 'qwerty'))
-//     done (null, "Test");
-//   }
-// ));
+const db = require('../models');
 
 passport.use(new LocalStrategy(
-  // Our user will sign in using a "email"
   {
-    usernameField: "email"
+    //Updating username field to email rather than default "username" from LocalStrategy
+    usernameField: 'email'
   },
-  function(email, password, done) {
-    // When a user tries to sign in this code runs
+  function (username, password, done) {
     db.User.findOne({
-      email: email
-    }).then(function(dbUser) {
-      // If there's no user with the given name
-      if (!dbUser) {
+      email: username
+    }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
         return done(null, false, {
-          message: "Incorrect username."
+          message: 'Incorrect username.'
         });
       }
-      // If there is a user with the given username, but the password the user gives us is incorrect
-      else if (!dbUser.authenticate()(password)) {
+      if (!user.validPassword(password)) {
         return done(null, false, {
-          message: "Incorrect password."
+          message: 'Incorrect password.'
         });
       }
-      // If none of the above, return the user
-      return done(null, dbUser);
+
+      return done(null, user);
+
     });
+
   }
 ));
-
 
 passport.serializeUser(db.User.serializeUser());
 passport.deserializeUser(db.User.deserializeUser());
 
 module.exports = passport;
-
