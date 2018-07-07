472f1c6c3b67fa85ca06fd23e8019b76fe2f82b2
Robert Shaw <robertshaw87@gmail.com>
2018-07-07 13:29:49 -0700

    add initial job actions logic

--- client/src/containers/AddJob/AddJob.js
+++ client/src/containers/AddJob/AddJob.js
@@ -1,41 +1,46 @@
-// import React, { Component } from 'react';
-// import AddJobForm from '../../components/AddJobForm/AddJobForm';
-// import { Container, Col, Row } from '../../components/Grid';
+import React, { Component } from 'react';
+import AddJobForm from '../../components/AddJobForm/AddJobForm';
+import { Container, Col, Row } from '../../components/Grid';
 
-// import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
-// import { addjob } from './actions';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addjob } from './actions';
 
-// class AddJob extends Component {
-//   addJob = values => {
-//     // This calls the addJob action creator, passing the form values to it 
-//    this.props.addJob(values);
-//   }
-//   render() {
-//     return (
-//       <Container>
-//         <Row>
-//           <Col />
-//           <Col size="12 md-8 lg-6" className="banana">
-//             <AddJobForm onSubmit={this.addJob} />
-//           </Col>
-//           <Col />
-//         </Row>
-//       </Container>
-//     )
-//   }
-// };
+class AddJob extends Component {
+  addJob = values => {
+    // This calls the addJob action creator, passing the form values to it 
+   this.props.addJob(values);
+  }
+  render() {
+    if (this.props.addJob.status) {
+      return <Redirect to='/' />
+    }
 
-// const mapStateToProps = (state,props) => {
-//   return { 
-//     addJob: state.addJob
-//   }
-// };
 
-// const mapActionsToProps = (dispatch,props) => {
-//   return bindActionCreators({
-//     addJob
-//   }, dispatch);
-// }
+    return (
+      <Container>
+        <Row>
+          <Col />
+          <Col size="12 md-8 lg-6">
+            <AddJobForm onSubmit={this.addJob} />
+          </Col>
+          <Col />
+        </Row>
+      </Container>
+    )
+  }
+};
 
-// export default connect(mapStateToProps,mapActionsToProps)(AddJob);
+const mapStateToProps = (state,props) => {
+  return { 
+    addJob: state.addJob
+  }
+};
+
+const mapActionsToProps = (dispatch,props) => {
+  return bindActionCreators({
+    addJob
+  }, dispatch);
+}
+
+export default connect(mapStateToProps,mapActionsToProps)(AddJob);
--- client/src/containers/AddJob/actions.js
+++ client/src/containers/AddJob/actions.js
@@ -1,50 +1,49 @@
-// import { signUp } from '../../utils/API';
-// import { LOGIN_SUCCESS } from '../Login/actions';
+import { createJob } from '../../utils/API';
  
-// export const FAILED_SIGNUP = 'FAILED_SIGNUP';
+export const ADDJOB_FAILED = 'ADDJOB_FAILED';
+export const ADDJOB_SUCCESS = 'ADDJOB_SUCCESS';
+export const ADDJOB_CLEAR = 'ADDJOB_CLEAR'
 
-// // Using Redux thunk middleware https://github.com/reduxjs/redux-thunk
-// // our action creator returns a function instead of an action. This function can
-// // be asynchronous to either delay the dispatch of an action, or dispatch only if certain
-// // conditions are met. In our case, we are having different actions dispatched depending on
-// // what part of the async process something is happening. This can be useful for transitional
-// // rendering, such as having a spinnging loading wheel while awaiting some data to be received from
-// // a DB query etc.
-// export function signup(userInfo) {
-//   // return a fn
-//   return async (dispatch, getState) => {
-//     try {
-//       const apiResponse = await (signUp(userInfo));
-//       // dispatch here
-//       dispatch(signedUp(apiResponse.data));
-//     } catch (err) {
-//       // and here
-//       dispatch(failedSignUp(err));
-//     }
-//   };
-// };
+// Using Redux thunk middleware https://github.com/reduxjs/redux-thunk
+// our action creator returns a function instead of an action. This function can
+// be asynchronous to either delay the dispatch of an action, or dispatch only if certain
+// conditions are met. In our case, we are having different actions dispatched depending on
+// what part of the async process something is happening. This can be useful for transitional
+// rendering, such as having a spinnging loading wheel while awaiting some data to be received from
+// a DB query etc.
+export function addJob(jobInfo) {
+  // return a fn
+  return async (dispatch, getState) => {
+    try {
+      await createJob(jobInfo);
+      // dispatch here
+      dispatch(successAddJob());
+    } catch (err) {
+      // and here
+      dispatch(failedAddJob(err));
+    }
+  };
+};
 
-// // We dispatch a type of LOGIN_SUCCESS because we want to log the user in
-// // if they successfully created an account. The Login reducer will pick up a LOGIN_SUCCESS.
-// export function signedUp(data) {
-//   return {
-//     type: LOGIN_SUCCESS,
-//     payload: {
-//       status: {
-//         firstName: data.firstName,
-//         lastName: data.lastName,
-//         email: data.email
-//       }
-//     }
-//   };
-// };
+export function successAddJob() {
+  return {
+    type: ADDJOB_SUCCESS,
+    payload: null
+  };
+};
 
-// export function failedSignUp(err) {
-//   return {
-//     type: FAILED_SIGNUP,
-//     payload: {
-//       status: false,
-//       error: err
-//     } 
-//   };
-// };
+export function failedAddJob(err) {
+  return {
+    type: ADDJOB_FAILED,
+    payload: {
+      error: err
+    } 
+  };
+};
+
+export function clearAddJob() {
+  return {
+    type: ADDJOB_CLEAR,
+    payload: null
+  }
+}
