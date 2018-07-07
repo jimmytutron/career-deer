8b285ab5b16937c4168277e1581d07cff9e678d7
Robert Shaw <robertshaw87@gmail.com>
2018-07-07 11:17:33 -0700

    first draft converting signup form to addjobs

---
+++ client/src/components/AddJobForm/AddJobForm.js
@@ -0,0 +1,55 @@
+import React from 'react';
+// Redux stuff
+import { Field, reduxForm, reset } from 'redux-form';
+import { TextField } from 'redux-form-material-ui';
+
+const renderTextField = ({
+  input,
+  label,
+  meta: { touched, error },
+  ...custom
+}) => (
+    <TextField
+      hintText={label}
+      floatingLabelText={label}
+      errorText={touched && error}
+      {...input}
+      {...custom}
+    />
+  )
+
+let AddJobForm = ({ handleSubmit, pristine, reset, submitting, errorMessage }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <Field name="firstName" component={renderTextField} type="text" label="First Name"></Field>
+      </div>
+      <div>
+        <Field name="lastName" component={renderTextField} type="text" label="Last Name"></Field>
+      </div>
+      <div>
+        <Field name="email" component={renderTextField} type="email" label="Email"></Field>
+      </div>
+      <div>
+        <Field name="password" component={renderTextField} type="password" label="Password"></Field>
+      </div>
+      <div>
+        <h6>{errorMessage}</h6>
+        <button type="submit" disabled={pristine || submitting}>
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+AddJobForm = reduxForm({
+  // a unique name for the form
+  form: 'addjob',
+  onSubmitSuccess: (result, dispatch) => dispatch(reset('addjob'))
+})(AddJobForm);
+
+// Inside this file, we wrapped our component inside the imported 'reduxForm' function
+// We can think of reduxForm() from redux-form behaving similar to connect() from react-redux in
+// terms of connecting a component to communicate with the store 
+export default AddJobForm
