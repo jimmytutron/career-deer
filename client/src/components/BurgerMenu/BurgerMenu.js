fabec1576e643781290f93cfb44cf0ce147cefe5
jimmytu <jimmytutron@gmail.com>
2018-07-10 15:25:46 -0700

    Style view jobs page

--- client/src/components/BurgerMenu/BurgerMenu.js
+++ client/src/components/BurgerMenu/BurgerMenu.js
@@ -9,11 +9,13 @@ class BurgerMenu extends React.Component {
             <ReduxBurgerMenu isOpen={ this.props.isOpen }>
             	<img width="50px" className="text-center my-3" src="/imgs/logo-white.svg" alt="menu logo"/>
                 <a id="home" className="menu-item" href="/">Home</a>
-                <a id="about" className="menu-item" href="/addjob">Add a Job</a>
-                <a id="contact" className="menu-item" href="/search">Search for Jobs</a>
-                <a id="contact" className="menu-item" href="/chart">View Charts</a>
+                <a id="add" className="menu-item" href="/addjob">Add a Job</a>
+                <a id="search" className="menu-item" href="/search">Search for Jobs</a>
+                <a id="view" className="menu-item" href="/viewjobs">Tracked Jobs</a>
+                <a id="board" className="menu-item" href="/board">Tracker Board</a>
+                <a id="charts" className="menu-item" href="/chart">View Charts</a>
                 <br/>
-                <a id="contact" className="menu-item" href="/">Logout</a>
+                <a id="logout" className="menu-item" href="/">Logout</a>
 
             </ReduxBurgerMenu>
         );
--- client/src/containers/ViewJobs/ViewJobs.js
+++ client/src/containers/ViewJobs/ViewJobs.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 import { Redirect } from "react-router-dom";
 import { Container, Col, Row } from '../../components/Grid';
 
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import './ViewJobs.css';
+
+import Zoom from 'react-reveal/Zoom';
+
 import { connect } from 'react-redux';
 import { editJob, resetViewJobs, getAllSavedJobs, updateViewJobs } from './actions';
+
+
 
 class ViewJobs extends Component {
 
@@ -16,26 +27,43 @@ class ViewJobs extends Component {
       return <Redirect to='/updateJob' />
     };
     return (
+      <React.Fragment>
       <Container className="mt-5">
         <Row className="justify-content-center">
           <Col size="12 md-12 lg-5">
-            View Jobs
+            <h1 className="text-center mb-5 montserrat font-weight-bold"><i className="fas fa-book-open"></i> Your Tracked Jobs</h1>
           </Col>
         </Row>
-        <Row className="justify-content-center mt-5">
-          <Col size="12 card">
+
+        <Row className="justify-content-center">
             {this.props.viewJobs.data.map((job, i) => (
-              <div className="my-3" key={`viewjob-${i}`}>
-                <h4>{job.title}</h4>
-                <p>{`company: ${job.company_name}`}</p>
-                <p>{job.post_date}</p>
-                <p>{`location: ${job.location}`}</p>
-                <button className="btn btn-light" onClick={() => this.props.editJob(this.props.viewJobs.data[i])}>{job._id}</button>
-              </div>
+        <Zoom>
+        <Card key={`viewjob-${i}`} className="my-2 mx-2 job-card">
+            <CardContent>
+                <Typography className="pt-2" color="textSecondary">
+                {job.company_name}
+                </Typography>
+                <Typography variant="headline" component="h2">
+                {job.title}
+                </Typography>
+                <Typography color="textSecondary">
+                {job.post_date}
+                </Typography>
+                <Typography component="p">
+                {job.location}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small" onClick={() => this.props.editJob(this.props.viewJobs.data[i])} data-id={job._id}>
+                <i className="fas fa-pen-square"></i> &nbsp; Edit
+                </Button>
+            </CardActions>
+        </Card>
+        </Zoom>
             ))}
-          </Col>
         </Row>
       </Container>
+      </React.Fragment>
     )
   }
 
