90b7111708add363d81a59187b0b83e2f7ba622b
jimmytu <jimmytutron@gmail.com>
2018-07-12 00:16:49 -0700

    Initial styling for job board

--- client/src/components/ProgressTile/ProgressTile.js
+++ client/src/components/ProgressTile/ProgressTile.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import { Droppable } from 'react-beautiful-dnd';
+import { Col } from '../../components/Grid';
 import JobTile from './JobTile';
+import './ProgessTile.css';
 
-const grid = 8;
 
 const getListStyle = isDraggingOver => ({
-  background: isDraggingOver ? 'lightblue' : 'lightgrey',
-  padding: grid,
-  width: 250
+  background: isDraggingOver ? 'lightblue' : 'white',
+  height: '100vh',
+  boxShadow: '0px 0px 1px #5B5B5B',
+  padding: '10px',
+  borderRadius: '10px',
+  overflow: 'scroll'
 });
 
 const ProgressTile = (key, jobs) => (
   <Droppable droppableId={key} key={key}>
     {(provided, snapshot) => (
-      <div>
-        <h1>{key}</h1>
+      <Col size="12 md-4 lg-2" className="mb-5">
+        <h1 className="text-center text-uppercase montserrat">{key}</h1>
         <div
           ref={provided.innerRef}
           style={getListStyle(snapshot.isDraggingOver)}>
@@ -25,9 +29,9 @@ const ProgressTile = (key, jobs) => (
               )
             })
           }
-          {/* {provided.placeholder} */}
+           {provided.placeholder}
         </div>
-      </div>
+      </Col>
     )}
   </Droppable>
 )
